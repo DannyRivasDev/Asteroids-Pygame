94bbc44968059796f32cab582bd5868b5acae21a
Danny <dannyrivasdev@gmail.com>
2024-01-02 00:03:50 -0500

    WIP

--- Asteroids/game.py
+++ Asteroids/game.py
@@ -1,12 +1,14 @@
 import pygame
 import random 
+from utils import load_sprite
 
 class Asteroids:
     def __init__(self):
         pygame.init()
         pygame.display.set_caption("Asteroids")
 
-        self.screen = pygame.display.set_mode((800, 600))
+        self.screen = pygame.display.set_mode((1080, 1080))
+        self.background = load_sprite("space", False)
     
     def main_loop(self):
         while True:
@@ -23,10 +25,9 @@ class Asteroids:
         pass
 
     def _draw(self):
-        self.screen.fill((0, 0, 128))
-        x = random.randint(10, 790)
-        y = random.randint(10, 590)
+        self.screen.blit(self.background, (0, 0))
+        x = random.randint(10, 1070)
+        y = random.randint(10, 1070)
         r = random.randint(20, 50)
         pygame.draw.circle(self.screen, (188, 188, 188), (x, y), r)
-        pygame.display.flip()
         pygame.display.flip()
