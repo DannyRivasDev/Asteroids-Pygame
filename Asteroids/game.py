3ed83b027b3fc213df66866c4c75eb4178072f98
Danny <dannyrivasdev@gmail.com>
2024-01-05 00:41:09 -0500

    WIP

--- Asteroids/game.py
+++ Asteroids/game.py
@@ -1,5 +1,6 @@
 import pygame
 import random 
+from models import GameObject
 from utils import load_sprite
 
 class Asteroids:
@@ -9,6 +10,12 @@ class Asteroids:
 
         self.screen = pygame.display.set_mode((1080, 1080))
         self.background = load_sprite("space", False)
+        self.spaceship = GameObject(
+            (400, 300), load_sprite("spaceship"), (0, 0)
+        )
+        self.asteroid = GameObject(
+            (400, 300), load_sprite("asteroid"), (1, 0)
+        )
     
     def main_loop(self):
         while True:
@@ -18,16 +25,17 @@ class Asteroids:
 
     def _handle_input(self):
         for event in pygame.event.get():
-            if event.type == pygame.QUIT:
+            if event.type == pygame.QUIT or (
+                event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE
+            ):
                 quit()
     
     def _game_logic(self):
-        pass
+        self.spaceship.move()
+        self.asteroid.move()
 
     def _draw(self):
         self.screen.blit(self.background, (0, 0))
-        x = random.randint(10, 1070)
-        y = random.randint(10, 1070)
-        r = random.randint(20, 50)
-        pygame.draw.circle(self.screen, (188, 188, 188), (x, y), r)
+        self.spaceship.draw(self.screen)
+        self.asteroid.draw(self.screen)
         pygame.display.flip()
