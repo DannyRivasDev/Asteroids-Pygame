e97909811f99ca7a99bbfc80c9446af2763ee385
Danny <dannyrivasdev@gmail.com>
2024-01-30 01:41:21 -0500

    Update

--- Asteroids/game.py
+++ Asteroids/game.py
@@ -21,7 +21,7 @@ class Asteroids:
         self.spaceship = Spaceship((540, 540), self.bullets.append)
 
         # Number of asteroids that spawn
-        for _ in range(8):
+        for _ in range(6):
             while True:
                 position = get_random_position(self.screen)
                 if (
